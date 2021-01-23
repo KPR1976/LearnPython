91115ff5458b6c16432ad25989c95a1f043d95d3
KPR <pkinyapin@gmail.com>
2021-01-23 20:57:12 +0200

    added Udemy course 2021 Complete Python Bootcamp From Zero to Hero in Python

---
+++ Complete-Python-3-Bootcamp/Capstone Project #1/tictactoe.py
@@ -0,0 +1,14 @@
+'''
+Tic Tac Toe game
+'''
+
+def desc(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+row1 = [' ', ' ', ' ']
+row2 = [' ', ' ', ' ']
+row3 = [' ', ' ', ' ']
+desc(row1, row2, row3)
---
+++ Complete-Python-3-Bootcamp/readme.md
@@ -0,0 +1,2 @@
+"2021 Complete Python Bootcamp From Zero to Hero in Python"
+https://www.udemy.com/course/complete-python-bootcamp
