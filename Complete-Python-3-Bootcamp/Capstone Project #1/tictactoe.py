cd4df39e672a21e1f0a3206e864d9e3170e2faef
KPR <pkinyapin@gmail.com>
2021-01-26 15:27:13 +0200

    1st version of tic tac toe

--- Complete-Python-3-Bootcamp/Capstone Project #1/tictactoe.py
+++ Complete-Python-3-Bootcamp/Capstone Project #1/tictactoe.py
@@ -1,70 +1,61 @@
 '''
-Tic Tac Toe game
+   |   |   
+ 1 | 2 | 3  
+   |   |   
+-----------
+   |   |   
+ 4 | 5 | 6  
+   |   |   
+-----------
+   |   |   
+ 7 | 8 | 9  
+   |   |   
+   
+
 '''
+symbols = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+finish = True
 
-def desc(row1, row2, row3):
-    print(row1)
-    print(row2)
-    print(row3)
+def field():
+    
+    for i in range(11):
 
-
-def user_choice():
-    #Initial conditions
-    choice = 'WRONG'
-    acceptable_range = range(0,10)
-    within_range = False
-
-
-    while choice.isdigit() == False or within_range == False:
-
-        choice = input('Please enter a number (0-10): ')
-
-        #check isdigit
-        if choice.isdigit() == False:
-            print("Sorry, but we work only with digits!")
-        #check range
-        else:
-            if int(choice) in acceptable_range:                
-                within_range = True
+            if i % 2 == 0:
+                print('   |   |   ')
+            elif i == 1:
+                print(' ' + symbols[0] + ' | ' + symbols[1] + ' | ' + symbols[2] + ' ')
+            elif i == 5:
+                print(' ' + symbols[3] + ' | ' + symbols[4] + ' | ' + symbols[5] + ' ')
+            elif i == 9:
+                print(' ' + symbols[6] + ' | ' + symbols[7] + ' | ' + symbols[8] + ' ')
             else:
-                print('Sorry, you are out of acceptable range (0-10)')
-                within_range = False
+                print('-----------')            
 
-    return int(choice)              
-
-def position_choice():
-    
-    # This original choice value can be anything that isn't an integer
-    choice = 'wrong'
-    
-    # While the choice is not a digit, keep asking for input.
-    while choice not in ['0','1','2']:
-        
-        # we shouldn't convert here, otherwise we get an error on a wrong input
-        choice = input("Pick a position to replace (0,1,2): ")
-        
-        if choice not in ['0','1','2']:
-            # THIS CLEARS THE CURRENT OUTPUT BELOW THE CELL
-            clear_output()
-            
-            print("Sorry, but you did not choose a valid position (0,1,2)")
-    
-    # We can convert once the while loop above has confirmed we have a digit.
-    return int(choice)
+field()
 
 
-def replacement_choice(game_list,position):
-    
-    user_placement = input("Type a string to place at the position")
-    
-    game_list[position] = user_placement
-    
-    return game_list    
+player1 = input('Choose your side X or O:')
+if player1 == 'X':
+    player2 = 'O'
+else:
+    player2 = 'X'      
 
-
-'''
-row1 = [' ', ' ', ' ']
-row2 = [' ', ' ', ' ']
-row3 = [' ', ' ', ' ']
-desc(row1, row2, row3)'''
-print(user_choice())
+for i in range(9):
+    if i % 2 == 0:
+        step = int(input('Player #1 choose a number 1-9:'))
+        symbols[step-1] = 'X'
+        field()
+        if symbols[0] == symbols[1] == symbols[2] == 'X' or symbols[0] == symbols[4] == symbols[8] == 'X' or symbols[0] == symbols[3] == symbols[6] == 'X' or \
+            symbols[3] == symbols[4] == symbols[5] == 'X' or symbols[6] == symbols[7] == symbols[8] == 'X' or symbols[1] == symbols[4] == symbols[7] == 'X' or \
+                symbols[2] == symbols[5] == symbols[8] == 'X' or symbols[2] == symbols[4] == symbols[6] == 'X':
+            print('You are winner Player #1!')
+            break
+    else:
+        step = int(input('Player #2 choose a number 1-9:'))
+        symbols[step-1] = 'O'
+        field()
+        if symbols[0] == symbols[1] == symbols[2] == 'O' or symbols[0] == symbols[4] == symbols[8] == 'O' or symbols[0] == symbols[3] == symbols[6] == 'O' or \
+            symbols[3] == symbols[4] == symbols[5] == 'O' or symbols[6] == symbols[7] == symbols[8] == 'O' or symbols[1] == symbols[4] == symbols[7] == 'O' or \
+                symbols[2] == symbols[5] == symbols[8] == 'O' or symbols[2] == symbols[4] == symbols[6] == 'O':
+            print('You are winner Player #2!')
+            break
