4fb04ad7a8227deb8232a0dea63022480650cd9d
KPR <pkinyapin@mail.com>
2021-05-29 12:38:55 +0300

    added replace_last exercise for o'relly section

---
+++ CheckIO/replace_last.py
@@ -0,0 +1,19 @@
+'''
+https://py.checkio.org/mission/replace-last/solve/
+In a given list the last element should become the first one. An empty list or list with only one element should stay the same
+'''
+def replace_last(line: list) -> list:
+    # your code here
+    line if len(line) <= 1 else line.insert(0, line.pop())
+    return line
+
+if __name__ == '__main__':
+    print("Example:")
+    print(replace_last([2, 3, 4, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert replace_last([2, 3, 4, 1]) == [1, 2, 3, 4]
+    assert replace_last([1, 2, 3, 4]) == [4, 1, 2, 3]
+    assert replace_last([1]) == [1]
+    assert replace_last([]) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
